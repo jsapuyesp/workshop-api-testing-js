6affe4e6db983683d19e9b0c98bf19a0a83b0629
Juan Sapuyes <jsapuyesp@gmail.com>
2022-05-04 16:08:41 -0500

    SchemaFile already(?)

--- schema/ListPublicEvents.schema.js
+++ schema/ListPublicEvents.schema.js
@@ -1,4 +1,14 @@
-const listPublicEventsSchema = {
-};
+const Schema = require('mongoose').Schema;
+const listPublicEventsSchema = new Schema({
+  id: String,
+  type: String,
+  actor: Object,
+  repo: Object,
+  payload: Object,
+  public: Boolean,
+  created_at: Date,
+  org: Object,
+  required: ['id', 'type', 'actor', 'repo', 'payload', 'public', 'created_at'],
+});
 
 exports.listPublicEventsSchema = listPublicEventsSchema;

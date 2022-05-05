09c59df02c73f3fa269b068980f2839d9286b74f
Juan Sapuyes <jsapuyesp@gmail.com>
2022-05-05 02:03:32 -0500

    GetById tested

--- test/ApiConsume.test.js
+++ test/ApiConsume.test.js
@@ -8,6 +8,9 @@ const urlBase = 'http://localhost:8080/api';
 // const githubUserName = 'jsapuyesp';
 // const repository = 'workshop-api-testing-js';
 
+// SE DEBE CORRER PRIMERO EL DOCKER
+// TENER AL MENOS UN OBJETO CREADO (POR AHORA)
+
 describe('Praxix Gildedrose API Test', () => {
   describe('Testing GET Services', () => {
     it('Consume GET, GET items', async () => {
@@ -16,13 +19,20 @@ describe('Praxix Gildedrose API Test', () => {
       expect(response.statusCode).to.equal(statusCode.OK);
     });
     // Añadir un segundo expect con el que se compruebe (creería yo) que la respuesta es una lista
+    it('Consume GET, GET item by id', async () => {
+      const response = await agent.get(`${urlBase}/items/4`);
+
+      expect(response.statusCode).to.equal(statusCode.OK);
+      expect(response.body).to.have.property('id').to.eql(4);
+    });
+    // Tener presente en el segundo expect cambiar el id que coincida con el que vamos a recibir
   });
   describe('Testing DELETE Services', () => {
     it('Consume DELETE, delete item by id', async () => {
-      const response = await agent.delete(`${urlBase}/items/3`);
+      const response = await agent.delete(`${urlBase}/items/4`);
 
       expect(response.status).to.equal(statusCode.OK);
-      expect(response.body).to.have.property('id').to.eql(3);
+      expect(response.body).to.have.property('id').to.eql(4);
     });
     // Tener presente en el segundo expect cambiar el id que coincida con el que vamos a eliminar
   });
@@ -30,7 +40,7 @@ describe('Praxix Gildedrose API Test', () => {
     it('Consume POST, creating item', async () => {
       const response = await agent.post(`${urlBase}/items`)
         .send({
-          name: 'Papa',
+          name: 'Sopa',
           sellIn: 12,
           quality: 25,
           type: 'AGED'
